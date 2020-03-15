edfbe4c17980e4430b23b78aa4138d892d5dcb42
Katia  Bellido <A01023638@itesm.mx>
2020-03-15 13:53:27 -0600

    editando clases

---
+++ sources/ArbolB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+
+template <class T>
+class arbolB{
+    public:
+    
+    void insertar();
+    void busqueda();
+    void eliminar();
+
+};
