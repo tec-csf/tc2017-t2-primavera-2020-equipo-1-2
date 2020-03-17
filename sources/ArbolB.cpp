49bbced1d4f541afafacb972f34e6e0c9cb30993
danielroa98 <daniel.roa98@gmail.com>
2020-03-17 14:33:55 -0600

    Added Switch and Time functions

--- sources/ArbolB.cpp
+++ sources/ArbolB.cpp
@@ -6,33 +6,76 @@
 using namespace std;
 using namespace std::chrono;
 
+
 template <class T>
 class arbolB{
 
-    
+    NodoArbol *raiz;    //Apuntador al nodo raíz
+    int grados;         // Valor que define cantidad de números que tendrá el bloque
 
-    public:
-    arbolB();
+public:
+    arbolB(int val);
     void insertar();
     void busqueda();
     void eliminar(int posKey)
-    {
-        int remover = 0; // aquí se va a agregar de buscar la posición en donde se encuentra
-
-        
-    }
-    void impresionValores();
+        {
+            int remover = 0; // aquí se va a agregar de buscar la posición en donde se encuentra
+        }
+        void impresionValores();
 
 };
 
 int main()
 {
 
-    int grados = 5;     // Valor que define cantidad de números que tendrá el bloque
+    int noGrados = 5;
     int cantValores;    // Cantidad de valores aleatorios que se insertarán en el Árbol
+    //arbolB grados(noGrados);   //Se predefinió que cada grupo va a alojar 5 números.
 
-    arbolB(grados);
+    cout << "¿Cuántos valores quieres que se ingresen al árbol?" << endl;
+    cout << "10\t100\t1000\t10000\t100000\t1000000" << endl;
 
+    cin >> cantValores;
+
+    auto start = high_resolution_clock::now();
+    switch (cantValores)
+    {
+    
+
+    case 10:
+        
+        break;
+    
+    case 100:
+
+        break;
+
+    case 1000:
+
+        break;
+
+    case 10000:
+
+        break;
+
+    case 100000:
+
+        break;
+
+    case 1000000:
+
+        break;
+
+    default:
+
+        cout << "Porfavor, seleccione una de las opciones disponibles" << endl;
+
+        break;
+    }
+
+    auto stop = high_resolution_clock::now();
+    auto durationMilli = duration_cast<milliseconds>(stop - start);
+    cout << "Le tomo " << durationMilli.count() << " milisegundos\n";
 
     return 0;
 }
